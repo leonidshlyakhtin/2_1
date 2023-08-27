0881e90030cfe133a4c23595a5da3a69afec81f0
elik <pochta@bk.ru>
2023-08-27 21:05:52 +0200

    2_1

---
+++ 1/Pair.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairClasses
+{
+    abstract class Pair
+    {
+       
+        abstract public Pair Add(Object obj);
+        abstract public Pair Sub(Object obj);
+        abstract public Pair Mult(double v);
+        abstract public bool CompareTo(Object obj);
+       
+        
+    
+        
+    }
+}
